318245ff804b1e485b08be8bf5cd6f74a6315daf
2134260 <tla.klaassen@student.han.nl>
2024-11-07 15:24:12 +0100

    menu made

--- applicatie/Menu.php
+++ applicatie/Menu.php
@@ -1,1 +1,35 @@
+<?php
+  require_once 'db_connect.php';
+  $query = 'SELECT "name", price FROM Product';
+  
+  try{
+    $db = maakVerbinding();
+    $data = $db->query($query);
+
+    $menu = "<table>";
+    $menu .= "<tr><th>Product</th><th>Prijs</th></tr>";
     
+    foreach ($data as $row) {
+      $product = $row['name'];
+      $price = $row['price'];
+    
+      $menu .= "<tr><td>$product</td><td>$price</td></tr>";
+    }
+    
+    $menu .= "</table>";
+  } catch (PDOException $e) {
+    $menu = "Error retrieving menu: " . $e->getMessage();
+  }
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+    </head>
+<body>
+    <?php echo $menu; ?>
+</body>
+</html>
