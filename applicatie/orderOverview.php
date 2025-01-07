4393ca061f99a9e6ac6215119a9855c792a3110d
2134260 <tla.klaassen@student.han.nl>
2025-01-07 23:17:33 +0100

    Update orderOverview.php

--- applicatie/orderOverview.php
+++ applicatie/orderOverview.php
@@ -26,10 +26,8 @@ $orders = getOrderOverview_P($_SESSION['username']);
       <li><a href="menu.php">Menu</a></li>
       <li><a href="winkelmandje.php">Winkelmandje</a></li>
       <li><a href="profile.php">Profiel</a></li>
-      <?php if ($_SESSION['role'] == 'Personnel'): ?>
-        <li><a href="orderOverview.php">bestelling overzicht</a></li>
-        <li><a href="detailOverview.php">Detail overzicht</a></li>
-      <?php endif; ?>
+      <li><a href="orderOverview.php">bestelling overzicht</a></li>
+      <li><a href="detailOverview.php">Detail overzicht</a></li>
       <li><a href="privacyverklaring.php">Privacyverklaring</a></li>
     </ul>
   </nav>
